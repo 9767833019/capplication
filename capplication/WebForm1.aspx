fb0a3b91ecd00aa6c8f2ebdd23c3750deea21328
Yogesh@8668 <yogeshpawar1512001@gmail.com>
2024-08-06 14:49:49 +0530

    Add project files.

---
+++ capplication/WebForm1.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="capplication.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Student Form</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblMessage" runat="server" ForeColor="Red"></asp:Label>
+            <br />
+            <asp:Label runat="server">Student Name</asp:Label>
+            <asp:TextBox ID="txtname" runat="server"></asp:TextBox>
+            <br />
+            <br />
+              <asp:Label runat="server">Student Address</asp:Label>
+  <asp:TextBox ID="txtAddress" runat="server"></asp:TextBox>
+  <br />
+  <br />
+              <asp:Label runat="server">Student Mob</asp:Label>
+  <asp:TextBox ID="txtmob" runat="server"></asp:TextBox>
+  <br />
+  <br />
+              <asp:Label runat="server">Student Age</asp:Label>
+  <asp:TextBox ID="txtage" runat="server"></asp:TextBox>
+  <br />
+  <br />
+            <asp:Button runat="server" ID="btn" Text="Insert" OnClick="btn_Click"/>
+            <br />
+
+           <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="StudentID" OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating" OnRowCancelingEdit="GridView1_RowCancelingEdit" OnRowDeleting="GridView1_RowDeleting">
+    <Columns>
+        <asp:BoundField DataField="StudentID" HeaderText="Student ID" ReadOnly="True" InsertVisible="False" />
+        <asp:TemplateField HeaderText="Name">
+            <ItemTemplate>
+                <asp:Label ID="lblName" runat="server" Text='<%# Eval("StudentName") %>'></asp:Label>
+            </ItemTemplate>
+            <EditItemTemplate>
+                <asp:TextBox ID="txtName" runat="server" Text='<%# Bind("StudentName") %>'></asp:TextBox>
+            </EditItemTemplate>
+        </asp:TemplateField>
+        <asp:TemplateField HeaderText="Address">
+            <ItemTemplate>
+                <asp:Label ID="lblAddress" runat="server" Text='<%# Eval("StudentAddress") %>'></asp:Label>
+            </ItemTemplate>
+            <EditItemTemplate>
+                <asp:TextBox ID="txtAddress" runat="server" Text='<%# Bind("StudentAddress") %>'></asp:TextBox>
+            </EditItemTemplate>
+        </asp:TemplateField>
+        <asp:TemplateField HeaderText="Age">
+            <ItemTemplate>
+                <asp:Label ID="lblAge" runat="server" Text='<%# Eval("StudentAge") %>'></asp:Label>
+            </ItemTemplate>
+            <EditItemTemplate>
+                <asp:TextBox ID="txtAge" runat="server" Text='<%# Bind("StudentAge") %>'></asp:TextBox>
+            </EditItemTemplate>
+        </asp:TemplateField>
+        <asp:CommandField ShowEditButton="True" ShowDeleteButton="True" />
+    </Columns>
+</asp:GridView>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ capplication/WebForm1.aspx.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace capplication
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        
+        private string connectionString = "Server=DESKTOP-11VRKLP\\SQLEXPRESS;Database=StudentDB;Integrated Security=true;";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGridView();
+            }
+        }
+
+        private void BindGridView()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Students", con))
+                {
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                }
+            }
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            BindGridView();
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            BindGridView();
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                int studentID = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value.ToString());
+                TextBox txtName = (TextBox)GridView1.Rows[e.RowIndex].FindControl("txtName");
+                TextBox txtAddress = (TextBox)GridView1.Rows[e.RowIndex].FindControl("txtAddress");
+                TextBox txtAge = (TextBox)GridView1.Rows[e.RowIndex].FindControl("txtAge");
+
+                if (txtName == null)
+                {
+                    lblMessage.Text = "Name textbox not found.";
+                    return;
+                }
+                if (txtAddress == null)
+                {
+                    lblMessage.Text = "Address textbox not found.";
+                    return;
+                }
+                if (txtAge == null)
+                {
+                    lblMessage.Text = "Age textbox not found.";
+                    return;
+                }
+
+                string name = txtName.Text.Trim();
+                string address = txtAddress.Text.Trim();
+                int age;
+
+                if (int.TryParse(txtAge.Text.Trim(), out age))
+                {
+                    using (SqlConnection con = new SqlConnection(connectionString))
+                    {
+                        using (SqlCommand cmd = new SqlCommand("UPDATE Students SET StudentName=@Name, StudentAddress=@Address, StudentAge=@Age WHERE StudentID=@StudentID", con))
+                        {
+                            cmd.Parameters.AddWithValue("@StudentID", studentID);
+                            cmd.Parameters.AddWithValue("@Name", name);
+                            cmd.Parameters.AddWithValue("@Address", address);
+                            cmd.Parameters.AddWithValue("@Age", age);
+                            con.Open();
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    GridView1.EditIndex = -1;
+                    BindGridView();
+                }
+                else
+                {
+                    lblMessage.Text = "Please enter a valid age.";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Text = "An error occurred: " + ex.Message;
+            }
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int studentID = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value.ToString());
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("DELETE FROM Students WHERE StudentID=@StudentID", con))
+                {
+                    cmd.Parameters.AddWithValue("@StudentID", studentID);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            BindGridView();
+        }
+
+        protected void btn_Click(object sender, EventArgs e)
+        {
+            string name = txtname.Text.Trim();
+            string address = txtAddress.Text.Trim();
+            int mob;
+            int age;
+
+            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(address) || !int.TryParse(txtage.Text.Trim(), out age) || !int.TryParse(txtmob.Text.Trim(),out mob))
+            {
+                lblMessage.Text = "Please fill out all fields correctly.";
+                return;
+            }
+            using (SqlConnection con=new SqlConnection(connectionString))
+            {
+                using(SqlCommand cd=new SqlCommand("INSERT INTO Students(StudentName,StudentAddress,StudentMob,StudentAge)VALUES(@Name,@Address,@Mob,@Age)",con))
+                {
+                    cd.Parameters.AddWithValue("@Name", name);
+                    cd.Parameters.AddWithValue("@Address", address);
+                    cd.Parameters.AddWithValue("@Mob", mob);
+                    cd.Parameters.AddWithValue("@Age", age);
+                    con.Open();
+                    cd.ExecuteNonQuery ();
+                    con.Close();
+                }
+                txtname.Text ="";
+                txtAddress.Text = "";
+                txtmob.Text = "";
+                txtage.Text = "";
+            }
+            BindGridView ();
+
+
+        }
+    }
+}
